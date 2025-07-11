35e4c6a16a923128067b235838f5c00886eb97bf
Zubair <zubairaslam43@gmail.com>
2025-07-11 18:57:51 +0500

    node v

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 - Sequelize ORM
 - MySQL
 - Dotenv for environment config
-
+- node v24.2.0
 # Clone the Repository
 - git clone https://github.com/mza43/user_post_category.git
 
--- config/config.js
+++ config/config.js
@@ -2,22 +2,22 @@ require('dotenv').config(); // Load .env variables
 
 module.exports = {
   development: {
-    username: process.env.DB_USER ,
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME ,
-    host: process.env.DB_HOST ,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'mysql'
   },
   test: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS || null,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql'
   },
   production: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS || null,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql'
