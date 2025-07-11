ee097a3469c6cdbaa56da20d711f362ef6f84edb
Zubair <zubairaslam43@gmail.com>
2025-07-11 19:39:30 +0500

    db port added

--- config/config.js
+++ config/config.js
@@ -6,6 +6,8 @@ module.exports = {
     password: process.env.DB_PASS || null,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT ,
+
     dialect: 'mysql',
   },
   test: {
@@ -13,6 +15,8 @@ module.exports = {
     password: process.env.DB_PASS || null,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT ,
+
     dialect: 'mysql',
   },
   production: {
@@ -20,6 +24,7 @@ module.exports = {
     password: process.env.DB_PASS || null,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT ,
     dialect: 'mysql',
   },
 };
--- config/db.js
+++ config/db.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT , 
+
 });
 
 // Test database connection
