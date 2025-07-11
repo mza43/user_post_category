e0fc36cdff72b7ed0a9db62c7b69fb76abd4b699
Zubair <zubairaslam43@gmail.com>
2025-07-11 17:55:50 +0500

    config.js and package.json update

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config(); // Load .env variables
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'root',
+    username: process.env.DB_USER ,
     password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || 'my_database',
-    host: process.env.DB_HOST || '127.0.0.1',
+    database: process.env.DB_NAME ,
+    host: process.env.DB_HOST ,
     dialect: 'mysql'
   },
   test: {
